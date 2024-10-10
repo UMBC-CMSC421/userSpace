56b4cf0ab8c7e3977ae4db67edcd8d3fc976f290
Sandra Deutl <sdeutl1@umbc.edu>
2024-10-10 01:26:53 -0400

    cloned and created READMEs; wrote inititalizer function and enqueue functions

--- deque.c
+++ deque.c
@@ -22,6 +22,26 @@ int init_deque(void)
     // TODO: Initialize all the pointers (high_head, high_tail, etc.) to NULL
     // TODO: Set all counts (high_count, medium_count, low_count) to zero
     // TODO: Initialize the semaphores and mutex
+    
+    deque = malloc(sizeof(struct deque_421_t));
+    deque->high_head = NULL;
+    deque->high_tail = NULL;
+    deque->medium_head = NULL;
+    deque->medium_tail = NULL;
+    deque->low_head = NULL;
+    deque->low_tail = NULL;
+    
+    high_count = 0;
+    medium_count = 0;
+    low_count = 0;
+    
+    //we want them to be shared by threads, so init with 0
+    sem_init(sem_high, 0, high_count);
+    sem_init(sem_medium, 0, medium_count);
+    sem_init(sem_low, 0, low_count);
+    
+    deque_mutex = PTHREAD_MUTEX_INITIALIZER;
+    
     return 0;
 }
 
@@ -37,6 +57,58 @@ int enqueue_front(char *data, int priority)
     // TODO: Insert the node at the front of the corresponding priority queue
     // TODO: Update the head and tail pointers as needed
     // TODO: Unlock the mutex and signal the corresponding semaphore
+    
+    //locking mutex
+    pthread_mutex_lock(deque_mutex);
+    
+    struct node *newNode = {NULL, NULL, &data}; //i feel like this is wrong
+    
+    if (priority == HIGH_PRIORITY) {
+    	if (deque->high_head == NULL) { //list is empty
+    		deque->high_head = newNode;
+    		//deque->high_head->next = deque-> high_tail;
+    		deque->high_tail = newNode;
+    	}
+    	else {
+    		newNode->next = deque->high_head;
+    		deque->high_head->prev = newNode;
+    		deque->high_head = newNode;
+    	}
+    	
+    	sem_signal(sem_high);
+    }
+    else if (priority == MEDIUM_PRIORITY) {
+    	if (deque->medium_head == NULL) {
+    		deque->medium_head = newNode;
+    		deque->medium_tail = newNode;
+    	}
+    	else {
+    		newNode->next = deque->medium_head;
+    		deque->medium_head->prev = newNode;
+    		deque->medium_head = newNode;
+    	}
+    	
+    	sem_signal(sem_medium); //do we need to call sem_wait?
+    }
+    else if (priority == LOW_PRIORITY) {
+    	if (deque->low_head == NULL) {
+    		deque->low_head = newNode;
+    		deque->low_tail = newNode;
+    	}
+    	else {
+    		newNode->next = deque->low_head;
+    		deque->low_head->prev = newNode;
+    		deque->low_head = newNode;
+    	}
+    	
+    	sem_signal(sem_low);
+    }
+    else {
+    	//that is an error
+    }
+    
+    //unlock
+    pthread_mutex_unlock(deque_mutex);
     return 0;
 }
 
@@ -48,6 +120,57 @@ int enqueue_front(char *data, int priority)
 int enqueue_rear(char *data, int priority)
 {
     // TODO: Implement similar logic as enqueue_front, but insert at the rear
+    
+    //locking mutex
+    pthread_mutex_lock(deque_mutex);
+    
+    struct node *newNode = {NULL, NULL, &data}; //i feel like this is wrong
+    
+    if (priority == HIGH_PRIORITY) {
+    	if (deque->high_head == NULL) { //list is empty
+    		deque->high_head = newNode;
+    		deque->high_tail = newNode;
+    	}
+    	else {
+    		newNode->prev = deque->high_tail;
+    		deque->high_tail->next = newNode;
+    		deque = high_tail = newNode;
+    	}
+    	
+    	sem_signal(sem_high);
+    }
+    else if (priority == MEDIUM_PRIORITY) {
+    	if (deque->medium_head == NULL) {
+    		deque->medium_head = newNode;
+    		deque->medium_tail = newNode;
+    	}
+    	else {
+    		newNode->prev = deque->medium_tail;
+    		deque->medium_tail->next = newNode;
+    		deque = medium_tail = newNode;
+    	}
+    	
+    	sem_signal(sem_medium); //do we need to call sem_wait?
+    }
+    else if (priority == LOW_PRIORITY) {
+    	if (deque->low_head == NULL) {
+    		deque->low_head = newNode;
+    		deque->low_tail = newNode;
+    	}
+    	else {
+    		newNode->prev = deque->low_tail;
+    		deque->low_tail->next = newNode;
+    		deque = low_tail = newNode;
+    	}
+    	
+    	sem_signal(sem_low);
+    }
+    else {
+    	//that is an error
+    }
+    
+    //unlock
+    pthread_mutex_unlock(deque_mutex);
     return 0;
 }
 
